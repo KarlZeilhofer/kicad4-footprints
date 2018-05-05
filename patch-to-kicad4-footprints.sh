e806657559f3684b3f0e8922fbf3211ddcc02f96
Karl Zeilhofer <zeilhofer@team14.at>
2018-05-05 12:55:35 +0200

    improved script with log
    set executable

--- patch-to-kicad4-footprints.sh
+++ patch-to-kicad4-footprints.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # apply patch, to use a path prefix for all models, 
-repren --from " \(model ([A-Za-z0-9])" --to " (model \${KISYS3DMOD}/\1" .
+repren --from " \(model ([A-Za-z0-9])" --to " (model \${KISYS3DMOD}/\1" ./*.pretty | tee -a log.txt
 rm */*.orig
 
 
 # and to use KICAD4_3D_DIR instead of KISYS3DMOD
-repren --from KISYS3DMOD --to KICAD4_3D_DIR .
+repren --from KISYS3DMOD --to KICAD4_3D_DIR ./*.pretty | tee -a log.txt
 rm */*.orig
 
 
